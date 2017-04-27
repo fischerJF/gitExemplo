099c935022439edb42a50e1e21cb42d84513d279
fischerJF <fischebatera@hotmail.com>
2017-04-27 20:00:16 -0300

    main update 2

--- src/gitExemplo/Main.java
+++ src/gitExemplo/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 	
 		System.out.println("teste");
+		System.out.println("teste1");
+		System.out.println("teste2");
+		System.out.println("teste3");
+		System.out.println("teste4");
+		
 	}
 
 }
